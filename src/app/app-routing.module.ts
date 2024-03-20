bad55bdf669e8e0d70e89f90d303602664b4f11a
JosephJohnson <JoeTestJohn@gmail.com>
2024-03-20 08:56:22 -0500

    first push to github

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,45 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListingComponent } from './product-listing/product-listing.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { CartManagementComponent } from './cart-management/cart-management.component';
+import { OrderHistoryComponent } from './order-history/order-history.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { LoginRegisterComponent } from './login-register/login-register.component';
+import { CategoryComponent } from './category/category.component';
 
-const routes: Routes = [];
+
+const routes: Routes = 
+[
+  {
+    path:"",
+    component:ProductListingComponent
+  },
+  {
+    path:"admin",
+    component:AdminDashboardComponent
+  },
+  {
+    path:"cart",
+    component:CartManagementComponent
+  },
+  {
+    path:"order",
+    component:OrderHistoryComponent
+  },
+  {
+    path:"categories",
+    component:CategoryComponent
+  },
+  {
+    path:"wishlist",
+    component:WishlistComponent
+  },
+  {
+    path:"login-register",
+    component:LoginRegisterComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,69 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginRegisterComponent } from './login-register/login-register.component';
+import { ProductListingComponent } from './product-listing/product-listing.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartManagementComponent } from './cart-management/cart-management.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { OrderHistoryComponent } from './order-history/order-history.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { MenuComponent } from './menu/menu.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginRegisterService } from './login-register.service';
+import { ProductService } from './product.service';
+import { ProductDetailsService } from './product-details.service';
+import { CategoryComponent } from './category/category.component';
+import { CategoryDetailsComponent } from './category-details/category-details.component';
+import { CategoryService } from './category.service';
+import { CartDetailsComponent } from './cart-details/cart-details.component';
+import { CartService } from './cart.service';
+import { WishlistDetailsComponent } from './wishlist-details/wishlist-details.component';
+import { WishlistService } from './wishlist.service';
+import { OrderService } from './order.service';
+import { OrderDetailsComponent } from './order-details/order-details.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { ProductAdminDetailsComponent } from './product-admin-details/product-admin-details.component';
+import { AdminService } from './admin.service';
+import { DiscountDetailsComponent } from './discount-details/discount-details.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
+@NgModule
+({
+  declarations: 
+  [
+    AppComponent,
+    LoginRegisterComponent,
+    ProductListingComponent,
+    ProductDetailsComponent,
+    CartManagementComponent,
+    WishlistComponent,
+    OrderHistoryComponent,
+    AdminDashboardComponent,
+    MenuComponent,
+    CategoryComponent,
+    CategoryDetailsComponent,
+    CartDetailsComponent,
+    WishlistDetailsComponent,
+    OrderDetailsComponent,
+    UserDetailsComponent,
+    ProductAdminDetailsComponent,
+    DiscountDetailsComponent,
   ],
-  imports: [
+  imports: 
+  [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: 
+  [
+    LoginRegisterService, ProductService, 
+    ProductDetailsService, CategoryService,
+    CartService, WishlistService,
+    OrderService, AdminService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
